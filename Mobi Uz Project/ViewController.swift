aa258ed134ecdd91ca6f9754249a558a47875aa7
Humoyun <kozimjonovhumoyun2509@gmail.com>
2022-08-06 14:01:22 +0500

    Initial Commit

---
+++ Mobi Uz Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobi Uz Project
+//
+//  Created by humoyun on 06/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
